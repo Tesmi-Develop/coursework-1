c442a0fda4828136633e711ffa28cb5af152e7c5
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2026-06-07 00:29:12 +1000

    fix: IntCustomConverter

--- Coursework1/JsonConverters/IntCustomConverter.cs
+++ Coursework1/JsonConverters/IntCustomConverter.cs
@@ -7,11 +7,13 @@ public class IntCustomConverter : JsonConverter<int>
 {
     public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType == JsonTokenType.Number)
-            if (reader.TryGetInt32(out int value))
-                return value;
+        if (reader.TokenType != JsonTokenType.Number || !reader.TryGetInt32(out var value)) 
+            throw new JsonException("Ожидалось число");
         
-        throw new JsonException("Ожидалось число");
+        if (value <= 0)
+            throw new JsonException("Число должно быть больше 0");
+        
+        return value;
     }
 
     public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
