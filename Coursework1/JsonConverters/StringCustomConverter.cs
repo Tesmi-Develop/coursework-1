8337b646a9a2acc056fcbe59834a058212fa6f02
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2026-06-07 00:35:22 +1000

    fix: string converter

--- Coursework1/JsonConverters/StringCustomConverter.cs
+++ Coursework1/JsonConverters/StringCustomConverter.cs
@@ -7,10 +7,12 @@ public class StringCustomConverter : JsonConverter<string>
 {
     public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType == JsonTokenType.String)
-            return reader.GetString() ?? string.Empty;
+        if (reader.TokenType != JsonTokenType.String)
+            throw new JsonException("Ожидалась строка");
         
-        throw new JsonException("Ожидалась строка");
+        var result = reader.GetString();
+
+        return string.IsNullOrEmpty(result) ? throw new JsonException("Строка не может быть пустой") : result;
     }
 
     public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
